5a7be3b12264f7b71a952b7adcd3c22c3c4f56f4
KaiqueOliveir0 <92691792+KaiqueOliveir0@users.noreply.github.com>
2026-05-28 00:02:26 -0300

    error MSG_RES

--- client.cpp
+++ client.cpp
@@ -257,7 +257,11 @@ int main(int argc, char* argv[]) {
         char secret_str[9];
 
         for (int i = 0; i < NA; i++) {
-            secret_str[i] = '0' + res_msg.a[i];
+            if (res_msg.a[i] >= 0 && res_msg.a[i] <= 9) {
+                secret_str[i] = '0' + res_msg.a[i];
+            } else {
+                secret_str[i] = (char)res_msg.a[i];
+            }
         }
 
         secret_str[NA] = '\0';
